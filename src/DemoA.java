f00352307dee2b98a7d95a22e694603891c7484a
madhu2810 <madhurids2892@gmail.com>
2019-06-04 13:37:48 +0530

    Commiting DemoB

--- src/DemoA.java
+++ src/DemoA.java
@@ -4,7 +4,9 @@ public class DemoA {
 	public static void main(String[] args) {
 		System.out.println("hii");
 		System.out.println("hello");
+		System.out.println("hiii");
 		System.out.println("Bye");
+		
 
 	}
 
